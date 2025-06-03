fd39db41aa747bc74d3af46dfb66b4d9818be3c8
Jesus Pedro Bapolo <jesusestudante@gmail.com>
2025-06-03 03:01:35 +0100

    add home page

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,8 +5,8 @@ function Nav() {
     return (
         <nav className="p-4 bg-blue-700 text-white w-full">
             <ul className="flex gap-4 text-md justify-end items-center px-4">
-                <li><Link to="/">home</Link></li>
-                <li><Link to="/cadastrar">cadastrar</Link></li>
+                <li><Link to="/home">home</Link></li>
+                <li><Link to="/">cadastrar</Link></li>
                 <li><Link to="/login">login</Link></li>
             </ul>
         </nav>
